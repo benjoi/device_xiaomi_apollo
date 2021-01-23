cf4f583c848e572db560576b239fd58706aebc27
ZenkaBestia <z.mail.6600@gmail.com>
2021-01-23 08:14:52 +0000

    lmi: update Nusantara-ify

--- nad_lmi.mk
+++ nad_lmi.mk
@@ -8,6 +8,9 @@ $(call inherit-product, device/xiaomi/lmi/device.mk)
 
 # Inherit some common Nusantara stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+NAD_BUILD_TYPE := OFFICIAL
+USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := nad_lmi
