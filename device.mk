b7d6c77aa4a8ac94b49b35147825387685d8068b
ZenkaBestia <z.mail.6600@gmail.com>
2021-01-24 02:10:58 +0000

    lmi: rename overlay-aicp to overlay-nusantara

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aicp
+    $(LOCAL_PATH)/overlay-nusantara
 
 # overlay-remove
 PRODUCT_PACKAGES += \
